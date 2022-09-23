2b21dc6852984f353515b72b52b793e792e0207c
Mr4K5 <113898911+ryantzyryan@users.noreply.github.com>
2022-09-23 12:04:57 +0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,33 @@
+import os,sys,requests,javascript,random
+from time import get 
+from requests import post
+from requests import get 
+
+K=0
+os.system("clear")
+print("Gratis Kuota Axisnet ( Axis.co.id )")
+number = input("628314979910 >")
+kuota = int(input("10GB >"))
+
+KUOTAGRATIS = {
+"Host:apis.google.com"
+"Conecction": "kept alive"
+"content-lenght": "36496"
+"accept": "javascript */*" 
+"user-agent": "Mozilla/5.0 (Linux; Android 9; RMX1941 Build/PPR1.180610.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/105.0.5195.136 Mobile Safari/537.36"
+Content-Type": "text/javascript; charset=UTF-8"
+"Sec-Fetch-Site": "cross-site"
+"Sec-Fetch-Mode": "no-cors"
+"Sec-Fetch-Dest": "script"
+"Referer": "https://www.google.co.in/"
+"Accept-Encoding": "gzip, deflate"
+"Accept-Language": "id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7"
+javascript.gratis({"identity":axis.co.id})"
+for i in range(10GB)
+    k += 1
+    pos =
+requests.post("https://apis.google.com/_/scs/abc-static/_/js/k=gapi.gapi.en.IK5OmUURd2E.O/m=gapi_iframes,googleapis_client/rt=j/sv=1/d=1/ed=1/rs=AHpOoo932JinkSJHK92WgVjIV-Jwwyu3Rw/cb=gapi.loaded_0"
+    if "success" in pos:
+       print("Kuota Gratis Berhasil",K)
+      else:
+       print("Kuota Gratis Gagal",K)
